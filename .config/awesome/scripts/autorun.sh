2672a03a9dfc090f1057f0a9e5b7cd1d8c977589
ties <ttjbloemen@gmail.com>
2024-01-30 17:19:15 +0100

    Add .config directory

---
+++ .config/awesome/scripts/autorun.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+function run {
+    if ! pgrep "$1" ; then
+        "$@"&
+    fi
+}
+
+# Specific to current laptop (HP Pavillion) and port (HDMI)
+if xrandr | grep -q "HDMI-1-0 connected" ; then
+    xrandr --output eDP --primary --mode 1920x1080 --pos 2560x691 --rotate normal --output HDMI-1-0 --mode 2560x1080 --pos 0x0 --rotate normal
+fi
