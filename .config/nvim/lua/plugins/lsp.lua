aa387427702cb96e6ce37613001b684bc97817ed
Ties Bloemen <t.t.j.bloemen@student.tudelft.nl>
2025-11-23 16:18:31 +0100

    add obsidian

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -5,6 +5,14 @@ return {
       servers = {
         pyright = {},
         ruff = {},
+        marksman = {
+          -- Enable marksman for markdown files
+          filetypes = { "markdown", "markdown.mdx" },
+          root_dir = function(fname)
+            local util = require("lspconfig.util")
+            return util.root_pattern(".obsidian", ".git")(fname)
+          end,
+        },
       },
       diagnostics = {
         virtual_text = false,
