2624b7c5799f4186ce3ff23b6671f1cf27351838
Ties Bloemen <t.t.j.bloemen@student.tudelft.nl>
2025-11-25 12:01:55 +0100

    add snacks (does not seem to be working right now)

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/snacks.nvim",
+  lazy = false,
+  priority = 100,
+  ---@type snacks.Config
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    picker = {
+      hidden = true,
+    },
+  },
+}
