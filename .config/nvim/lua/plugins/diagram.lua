a1f81c04568ed8da85453707f5f96afc92f45c69
Ties Bloemen <t.t.j.bloemen@student.tudelft.nl>
2025-12-06 13:53:06 +0100

    add diagram.nvim

---
+++ .config/nvim/lua/plugins/diagram.lua
@@ -0,0 +1,8 @@
+return {
+  "3rd/diagram.nvim",
+  dependencies = {
+    { "3rd/image.nvim" }, -- you'd probably want to configure image.nvim manually instead of doing this
+  },
+  opts = { -- you can just pass {}, defaults below
+  },
+}
