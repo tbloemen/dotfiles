4ad06af861a08f9f45dfedd8855753e11183d186
Ties Bloemen <t.t.j.bloemen@student.tudelft.nl>
2025-12-16 17:36:27 +0100

    delete diagram

--- .config/nvim/lua/plugins/diagram.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "3rd/diagram.nvim",
-  dependencies = {
-    { "3rd/image.nvim" }, -- you'd probably want to configure image.nvim manually instead of doing this
-  },
-  opts = { -- you can just pass {}, defaults below
-  },
-}
