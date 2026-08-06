d5ff99fe7a45be3263371ecdeef283f20a9a7b6c
Ties Bloemen <t.t.j.bloemen@student.tudelft.nl>
2026-08-06 13:18:23 +0200

    fix beancount syntax highlighting

--- .config/nvim/lua/plugins/beancount.lua
+++ .config/nvim/lua/plugins/beancount.lua
@@ -1,32 +1,35 @@
 return {
-  "hxueh/beancount.nvim",
-  ft = { "beancount", "bean" },
-  dependencies = {
-    {
-      "saghen/blink.cmp",
-      optional = true,
-      opts = function(_, opts)
-        table.insert(opts.sources.default, "beancount")
-        opts.sources.providers = opts.sources.providers or {}
-        opts.sources.providers.beancount = {
-          name = "beancount",
-          module = "beancount.completion.blink",
-          score_offset = 100,
-          opts = {
-            trigger_characters = { ":", "#", "^", '"', " " },
-          },
-        }
-        return opts
-      end,
-    },
-    {
-      "L3MON4D3/LuaSnip",
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "beancount" } },
   },
-  config = function()
-    require("beancount").setup({})
-    require("nvim-treesitter").setup({
-      ensure_installed = { "beancount" },
-    })
-  end,
+  {
+    "hxueh/beancount.nvim",
+    ft = { "beancount", "bean" },
+    dependencies = {
+      {
+        "saghen/blink.cmp",
+        optional = true,
+        opts = function(_, opts)
+          table.insert(opts.sources.default, "beancount")
+          opts.sources.providers = opts.sources.providers or {}
+          opts.sources.providers.beancount = {
+            name = "beancount",
+            module = "beancount.completion.blink",
+            score_offset = 100,
+            opts = {
+              trigger_characters = { ":", "#", "^", '"', " " },
+            },
+          }
+          return opts
+        end,
+      },
+      {
+        "L3MON4D3/LuaSnip",
+      },
+    },
+    config = function()
+      require("beancount").setup({})
+    end,
+  },
 }
