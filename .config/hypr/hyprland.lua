370bca1e7e81499e043e2aebb472f8257379f4ee
Ties Bloemen <t.t.j.bloemen@student.tudelft.nl>
2026-08-14 13:44:54 +0200

    update border color

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -135,7 +135,7 @@ hl.config({
 		border_size = 2,
 
 		col = {
-			active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
+			active_border = { colors = { "#cba6f7", "#cba6f7" }, angle = 45 },
 			inactive_border = "rgba(595959aa)",
 		},
 
