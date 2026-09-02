4ec8af1939517b7378ac84f744f66a71ac3a08ad
Ties Bloemen <t.t.j.bloemen@student.tudelft.nl>
2026-09-02 11:37:26 +0200

    disable image preview in text

--- .config/nvim/lua/plugins/image.lua
+++ .config/nvim/lua/plugins/image.lua
@@ -1,20 +1,22 @@
-return {
-  {
-    "vhyrro/luarocks.nvim",
-    priority = 1001,
-    opts = {
-      rocks = { "magick" },
-    },
-  },
-  {
-    "3rd/image.nvim",
-    dependencies = { "luarocks.nvim" },
-    config = function()
-      require("image").setup({
-        backend = "kitty",
-        kitty_method = "normal",
-        integrations = {},
-      })
-    end,
-  },
-}
+return {}
+
+-- return {
+--   {
+--     "vhyrro/luarocks.nvim",
+--     priority = 1001,
+--     opts = {
+--       rocks = { "magick" },
+--     },
+--   },
+--   {
+--     "3rd/image.nvim",
+--     dependencies = { "luarocks.nvim" },
+--     config = function()
+--       require("image").setup({
+--         backend = "kitty",
+--         kitty_method = "normal",
+--         integrations = {},
+--       })
+--     end,
+--   },
+-- }
